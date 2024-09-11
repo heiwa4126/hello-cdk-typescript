5262dfbf03411dbef11b5ce260213fa8cd6a65cf
heiwa4126 <heiwa4126@gmail.com>
2024-09-11 13:55:43 +0900

    add lambda.Code.fromAsset() example

---
+++ lambda/hello/tests/unit/test-handler.mjs
@@ -0,0 +1,18 @@
+// import { expect } from "chai";
+// import { lambdaHandler } from "../../app.mjs";
+// var event, context;
+
+// describe("Tests index", () => {
+// 	it("verifies successful response", async () => {
+// 		const result = await lambdaHandler(event, context);
+
+// 		expect(result).to.be.an("object");
+// 		expect(result.statusCode).to.equal(200);
+// 		expect(result.body).to.be.an("string");
+
+// 		const response = JSON.parse(result.body);
+
+// 		expect(response).to.be.an("object");
+// 		expect(response.message).to.be.equal("hello world");
+// 	});
+// });
--- lib/hello-cdk-typescript-stack.ts
+++ lib/hello-cdk-typescript-stack.ts
@@ -1,25 +1,32 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import type { Construct } from "constructs";
-
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path = require("node:path");
 
 export class HelloCdkTypescriptStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
 		// Define the Lambda function resource
+		// const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+		// 	runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+		// 	handler: "index.handler",
+		// 	code: lambda.Code.fromInline(`
+		//     exports.handler = async function(event) {
+		//       return {
+		//         statusCode: 200,
+		//         body: JSON.stringify('Hello CDK!'),
+		//       };
+		//     };
+		//   `),
+		// });
+
 		const myFunction = new lambda.Function(this, "HelloWorldFunction", {
 			runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
-			handler: "index.handler",
-			code: lambda.Code.fromInline(`
-        exports.handler = async function(event) {
-          return {
-            statusCode: 200,
-            body: JSON.stringify('Hello CDK!'),
-          };
-        };
-      `),
+			handler: "app.lambdaHandler",
+			code: lambda.Code.fromAsset(path.join(__dirname, "..", "lambda", "hello"), {
+				exclude: ["*.log", "tests/**/*"],
+			}),
 		});
 
 		// Define the Lambda function URL resource
