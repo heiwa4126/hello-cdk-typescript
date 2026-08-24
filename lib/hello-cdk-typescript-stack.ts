3e730a80f15a44c7b80e750a313c84ad376b666a
heiwa4126 <heiwa4126@gmail.com>
2026-08-24 14:00:49 +0900

    update modules

--- lib/hello-cdk-typescript-stack.ts
+++ lib/hello-cdk-typescript-stack.ts
@@ -12,25 +12,25 @@ export class HelloCdkTypescriptStack extends cdk.Stack {
 			runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
 			handler: "app.lambdaHandler",
 			code: lambda.Code.fromAsset("./lambda/hello", {
-				exclude: ["*.log", "tests/**/*"],
-			}),
+				exclude: ["*.log", "tests/**/*"]
+			})
 		});
 
 		// Create a CloudWatch Logs Log Group for myFunction
 		new logs.LogGroup(this, "HelloWorldFunctionLogGroup", {
 			retention: logs.RetentionDays.ONE_WEEK,
 			logGroupName: `/aws/lambda/${myFunction.functionName}`,
-			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			removalPolicy: cdk.RemovalPolicy.DESTROY
 		});
 
 		// Define the Lambda function URL resource
 		const myFunctionUrl = myFunction.addFunctionUrl({
-			authType: lambda.FunctionUrlAuthType.NONE,
+			authType: lambda.FunctionUrlAuthType.NONE
 		});
 
 		// Define a CloudFormation output for your URL
 		new cdk.CfnOutput(this, "HelloFunctionUrl", {
-			value: myFunctionUrl.url,
+			value: myFunctionUrl.url
 		});
 		// // Define a CloudFormation output for LogGroup
 		// new cdk.CfnOutput(this, "HelloFunctionLogGroup", {
